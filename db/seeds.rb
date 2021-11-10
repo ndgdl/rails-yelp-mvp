fbea4cc75f0d973fb4306763ade6245b4047d2ea
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-10 14:17:15 +0100

    Working Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+puts "Cleaning done!"
+
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Seeding..."
+
+restaurant_categories = %w[chinese italian japanese french belgian]
+
+10.times do
+  new_restaurant = Restaurant.new(
+                    name: Faker::Restaurant.name,
+                    address: Faker::Address.full_address,
+                    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+                    category: restaurant_categories.sample
+                  )
+
+  new_restaurant.save
+
+  rand(1..10).times do
+    new_review = Review.new(
+                  content: Faker::Restaurant.review,
+                  rating: rand(0..5),
+                  restaurant_id: new_restaurant.id
+                )
+    new_review.save
+  end
+end
+puts 'Seeding done!'
+puts "#{Restaurant.count} restaurants created"
+puts "#{Review.count} reviews created"
